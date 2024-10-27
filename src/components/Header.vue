b816d650552b2a875de14bf7ba8deee3cbec62d8
benj12 <44120311+benj12@users.noreply.github.com>
2024-10-27 01:26:35 -0400

    Added home.css and header.css files

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="header">
-        <h1>Welcome to Mafia Detected!</h1>
+        <h1 id="header-title">Welcome to Mafia Detected!</h1>
         <p><em>Become a mafia member, doctor, detective, or townsman and play the thrilling game now!</em></p>
         <router-link to="/signup">
             <button class="spacing-between-buttons">Sign Up</button>
@@ -12,10 +12,10 @@
 
         <h2 class="role-header"><strong>Obtain Your Role</strong></h2>
         <div class="roles-background">
-            <span :class="['highlight', 'rounded']">Mafia</span>
-            <span :class="['highlight', 'rounded']">Doctor</span>
-            <span :class="['highlight', 'rounded']">Detective</span>
-            <span :class="['highlight', 'rounded']">Townsman</span>
+            <span :class="['highlight', 'rounded-card']">Mafia</span>
+            <span :class="['highlight', 'rounded-card']">Doctor</span>
+            <span :class="['highlight', 'rounded-card']">Detective</span>
+            <span :class="['highlight', 'rounded-card']">Townsman</span>
         </div>
         <p class="role-text"><em>Use your role wisely to outsmart others in the game.</em></p> 
     </header>
@@ -28,5 +28,5 @@ export default {
 </script>
 
 <style scoped>
-@import "../css/header.css";
+@import '../css/header.css';
 </style>
